a539290aaa370a68334aa1f6764b235e57d366fc
João Vitor De Lima <88281625+joaolima16@users.noreply.github.com>
2023-07-19 13:53:12 -0300

    Add files via upload

--- src/Config/Database.ts
+++ src/Config/Database.ts
@@ -1,12 +1,11 @@
 import { Sequelize } from 'sequelize'
 import path from 'path'
 require("dotenv").config({path: path.resolve(__dirname, "../../.env")})
-
+console.log(process.env.USER)
 export const conn =  new Sequelize({
-    database: process.env.DATABASE,
-    username: process.env.USERNAME,
-    password: '',
+    database: process.env.DATABASE ,
+    username: process.env.USER,
+    password: process.env.PASSWORD,
     host: process.env.HOST,
-    port: 3306,
     dialect: "mysql"
 })
